278dc25f561f02bf8bc86cd13d8f72adaa7186f2
fokkinkniels <fokkinkniels@gmail.com>
2020-06-04 17:01:27 +0200

    added bootstrap to account page, not done yet!

--- account.php
+++ account.php
@@ -1,6 +1,9 @@
 <?php
-    session_start();
-	include __DIR__ .'/classes/autoLoader.class.php';
+    require 'includes/header.php';	
+
+    if(!isset($_SESSION['userId'])){
+        header("Location: ./login.php");
+    }
 
     if(isset($_POST["submit-upload"])){
 
@@ -14,64 +17,34 @@
             exit();  
         }
     }
+
+    $userView = new UsersView();
+
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account</title>
-
-    <link rel="stylesheet" type="text/css" href="css/styleAccount.css">
-
-</head>
-<body class="blackbg">
-
-    <?php
-        include './includes/header.php';
-    ?> 
-    <div class='row no-gutters'>
-        <div class="col-md-10 offset-1">
-
+    <!-- form to upload new profile image -->
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">                   
+        <div class="row no-gutters p-1">
+            <div class="file inputbox col-md-10 p-5" id="yourBtn" for="file" onclick="getFile()">click to select a new profile image</div>
+            <div style="height: 0px; width: 0px; overflow: hidden;">
+                <input id="file" name="file" type="file" onchange="sub(this)">
+            </div> 
+        </div> 
         <?php
-
-
-                if(isset($_SESSION['userId'])){
-
-                    $userView = new UsersView();
-                    $userView->loadProfile($_SESSION['userName']);
-
-                    echo '
-                    <form action="'.$_SERVER['PHP_SELF'].'" method="POST" enctype="multipart/form-data">
-                    
-                    <div class="row no-gutters">
-                        <div class="file col-md-12" id="yourBtn" for="file" onclick="getFile()">click to upload a new profile image</div>
-                        <div style="height: 0px; width: 0px; overflow: hidden;">
-                            <input id="file" name="file" type="file" onchange="sub(this)">
-                        </div> 
-                    </div> 
-                        <button class="col-md-6 DefaultBtn" type="submit" name="submit-upload">Upload New Image</button>
-                    </form>';
-
-                    if(!empty($errors)){
-                        foreach($errors as $error){
-                            echo'<div class="errorMessage">';
-                            echo $error;
-                            echo '</div>';
-                        }
-                    }
-                    echo '
-                        <a href="updateProfile.php"><button class="col-md-6 updateProfile DefaultBtn">Change Profile</button></a>
-                    ';
+            if(!empty($errors)){
+                foreach($errors as $error){
+                    echo'<div class="text-danger col-md-12 text-center p-1">';
+                    echo $error;
+                    echo '</div>';
                 }
+            }
+        ?>
+        <button class="DefaultBtnYel col-md-8 p-1 offset-2" type="submit" name="submit-upload">Upload New Image</button>
+    </form>
 
-            ?>
-        </div>
-    </div>
+
+
 <script type="text/javascript" src="/scripts/files.js"></script>
-
-    
+   
 </body>
 </html>
